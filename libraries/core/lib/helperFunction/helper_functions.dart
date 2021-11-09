4376c07c74160c8bed791a22b2ae355b6f17fb9d
Muhammad Zubair <Muhammad.Zubair@iamplus.com>
2021-11-09 20:15:01 +0500

    Removed Unused files

--- libraries/core/lib/helperFunction/helper_functions.dart
+++ libraries/core/lib/helperFunction/helper_functions.dart
@@ -1,9 +1,14 @@
-import 'package:core/core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:widgets/widgets.dart';
-import 'dart:math' show cos, sqrt, asin;
+
 import 'package:dio/dio.dart';
+
+
+bool isNullOrEmpty(String text) {
+  if (text == null || text.isEmpty)
+    return true;
+  else
+    return false;
+}
+
 
 bool isValidResponse(Response response) {
   return (response != null ||
--- libraries/widgets/lib/widgets.dart
+++ libraries/widgets/lib/widgets.dart
@@ -1,13 +1,9 @@
 library widgets;
-
 export 'button.dart';
 export 'text.dart';
-export 'info_dialog.dart';
 export 'loading.dart';
-
 export 'icon_button.dart';
-export 'card_button.dart';
 export 'image_loading_shimmer.dart';
-export 'flutter_svg_service.dart';
+
 
 
